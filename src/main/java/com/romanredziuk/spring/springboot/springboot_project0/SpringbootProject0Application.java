db0a2e8282965844ce33b04776862cbbc432fbd5
Roman Redziuk <r8mano@gmail.com>
2022-10-27 13:34:34 +0100

    init commit

---
+++ src/main/java/com/romanredziuk/spring/springboot/springboot_project0/SpringbootProject0Application.java
@@ -0,0 +1,13 @@
+package com.romanredziuk.spring.springboot.springboot_project0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProject0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/romanredziuk/spring/springboot/springboot_project0/SpringbootProject0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanredziuk.spring.springboot.springboot_project0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
